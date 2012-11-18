4204ea3adfbc9b8e0d4350e251e2c00becb975f7
Fred Wenzel <fwenzel@mozilla.com>
2012-11-18 13:27:24 -0800

    Copy Dropbox URL to clipboard automatically.

--- upshot.py
+++ upshot.py
@@ -5,9 +5,12 @@ import os
 import shutil
 import sys
 import time
+import urllib
+import urlparse
 
+import Foundation
+from AppKit import NSArray, NSPasteboard
 
-LOG_LEVEL = logging.DEBUG
 
 # XXX: Use (possibly) configured screen shot dir, fall back to desktop.
 # http://secrets.blacktree.com/?showapp=com.apple.screencapture
@@ -16,6 +19,12 @@ SCREENSHOT_DIR = os.path.join(os.environ['HOME'], 'Desktop')
 SHARE_DIR = os.path.join(os.environ['HOME'], 'Dropbox', 'Public',
                          'Screenshots')
 
+# XXX: Detect this, somehow
+DROPBOX_ID = 18779383  # Part of my public shares, probably safe to put here.
+SHARE_URL = 'http://dl.dropbox.com/u/%s/Screenshots/' % DROPBOX_ID
+
+# Set up logging
+LOG_LEVEL = logging.DEBUG
 logging.basicConfig(level=LOG_LEVEL)
 log = logging.getLogger('upshot')
 
@@ -29,6 +38,22 @@ def handle_screenshot(imagefile):
     # Move image file to target dir.
     log.debug('Moving %s to %s' % (imagefile, SHARE_DIR))
     shutil.move(imagefile, SHARE_DIR)
+
+    # Create shared URL
+    url = urlparse.urljoin(
+        SHARE_URL, urllib.quote(os.path.basename(imagefile)))
+    logging.debug('Share URL is %s' % url)
+
+    logging.debug('Copying to clipboard.')
+    pbcopy(url)
+
+
+def pbcopy(s):
+    """Copy text to the OS X clipboard."""
+    pb = NSPasteboard.generalPasteboard()
+    pb.clearContents()
+    a = NSArray.arrayWithObject_(s)
+    pb.writeObjects_(a)
 
 
 if __name__ == '__main__':
